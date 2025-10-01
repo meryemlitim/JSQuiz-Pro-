1735f0432e306334e62a9dbbda25113492f73b04
meryemlitim <litimmeryem2001@gmail.com>
2025-10-01 14:30:55 +0100

    authentification style

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes/routes');
+const session = require("express-session");
 
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
@@ -14,22 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static files (CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use routes
-app.use('/', routes);
-
-// Start server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});const express = require("express");
-const session = require("express-session");
-const authRoutes = require("./routes/authRoutes");
-
-
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
 
 // Session
 app.use(session({
@@ -38,9 +23,11 @@ app.use(session({
   saveUninitialized: false
 }));
 
-// Routes
-app.use("/", authRoutes);
+// Use routes
+app.use('/', routes);
 
-app.listen(3000, () => {
-  console.log("✅ Server running on http://localhost:3000");
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,24 @@ const express = require("express");
 
 // require the express router
 const router = express.Router();
+
+const authController = require("../controllers/authController");
 const userController = require("../controllers/userController");
 
+
+// Auth
+router.get("/", authController.showRegister);
+router.get("/register", authController.showRegister);
+router.post("/register", authController.register);
+
+router.get("/login", authController.showLogin);
+router.post("/login", authController.login);
+
+router.get("/logout", authController.logout);
+
+
 // set the routes
-router.get("/", userController.index);
+router.get("/home", userController.index);
 router.get("/quiz", userController.quiz);
 router.post("/quiz", userController.submitQuiz);
 
